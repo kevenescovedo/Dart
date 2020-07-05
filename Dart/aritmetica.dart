7bc6b9ed37e0343441e7cd18dc90c69c414ae947
kevenescovedo <49500205+kevenescovedo@users.noreply.github.com>
2020-07-05 18:07:26 -0300

    Add files via upload

---
+++ Dart/aritmetica.dart
@@ -0,0 +1,34 @@
+void main() {
+  double num1 = 10.0;
+  double num2 = 10.0;
+  double resultado = num1 + num2;
+  double num3 = 14;
+  double num4 = 7;
+  print("A divsão de $num3 por $num4 é igual á $divisao");
+  print("A soma de $num1 mais $num2 é igual á $resultado");
+  double multiplicacao = num1 * num2;
+  print("A multiplicação de $num1 mais $num2 é igual á $multiplicacao");
+
+
+  double divisao = num3 / num4;
+
+ double subtracao = num3 - num2;
+ print("A subtração de $num3 e $num2 é igual á $subtracao");
+ var inversao = -(-5);
+ print('o resultado de inversão é ${inversao}'); 
+ var divisao_resultado_inteiro = 5 ~/ 2;
+ print('o resultado inteiro de divisão é $ {divisao_resultado_inteiro}');  
+ 
+// modulo é igual a 1
+var modulo = 9 % 2;
+print('o resto de divisão(modulo) é $ {modulo}');  
+   int a = 0;
+   int b = 0;
+   int c = 0;
+   int delta = 
+
+
+
+
+ double delta = 
+}
---
+++ Dart/dart1.dart
@@ -0,0 +1,18 @@
+void main() {
+  String nome = 'Loginha do Keven';
+  int bananas = 5;
+  double preco = 2.50;
+  bool aindaTembananas = true;
+  print(nome);
+  nome = "Loginha escovedo";
+  print("O nome da loginha é: " + nome);
+  if (aindaTembananas) {
+    print("Possui $bananas bananas");
+  } else {
+    print("não tem bananas");
+   
+
+  }
+  var teste = "teste"
+  print(teste)
+}
